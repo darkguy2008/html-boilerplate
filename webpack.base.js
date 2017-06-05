06fbbd0ef809aa6303c9908abdbe66d103152739
DARKGuy <darkguy2008@gmail.com>
2017-06-05 17:45:11 -0600

    Webpack improvements and package updates

--- webpack.base.js
+++ webpack.base.js
@@ -1,52 +1,38 @@
 const webpack = require('webpack');
+var HappyPack = require('happypack');
 const path = require('path');
 
 module.exports = function() {
   return {
     entry: {
-      main: './main.js',
+      main: path.resolve(__dirname, './main.js')
     },
     output: {
       path: path.resolve(__dirname, '.'),
+      publicPath: '/',
       filename: '[name].min.js'
     },
+    context: __dirname,
+
     cache: true,
     watchOptions: {
       poll: 250
     },
+
     module: {
       noParse: [
         /(node_modules|~)\/(jquery)\//gi
       ],
       loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: [ 'babel-loader?presets[]=es2015' ]
-      },
-      {
-        test: /\.html$/,
-        loader: "html-loader"
-      },
-      { 
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-        loader: "url-loader?limit=10000&mimetype=application/font-woff" 
-      },
-      { 
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-        loader: "file-loader" 
-      },
-      {
-        test: /\.less$/,
-        loader: "style-loader!css-loader!less-loader"
-      },
-      {
-        test: /\.(png|jpg)$/,
-        loader: "url-loader"
-      }
+        { test: /\.js$/, loader: 'happypack/loader?id=js', exclude: [/node_modules/, /scaffold/, /dist/] },
+        { test: /\.html$/, loader: "html-loader" },
+        { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/, loader: 'url-loader' },
+        { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
+        { test: /\.(png|jpg)$/, loader: "url-loader" }
       ]
     },
     plugins: [
+      new HappyPack({ id: 'js', loaders: ['babel-loader?presets[]=es2015&cacheDirectory=true'] }),
       new webpack.optimize.OccurrenceOrderPlugin(true),
       new webpack.ProvidePlugin({
         $: "jquery",
